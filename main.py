10d74bd385baa222c20d5294378a47e7c1b83592
Damir <YWorker@yandex.ru>
2023-10-12 22:44:22 +0300

    added del_entry

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ class Main:
                 self.entries_exp()
             if action == 4:
                 self.add_entry()
+            if action == 5:
+                self.del_entry()
         except ValueError:
             print('Invalid action specified! Return to the main menu.')
             Main.__init__(self)
